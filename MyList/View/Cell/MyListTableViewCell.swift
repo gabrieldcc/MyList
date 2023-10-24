d4881da6cb7c4dc3e356942d7e1853b3f2f1866d
Gabriel Castro <gabrieldcc@gmail.com>
2023-10-24 19:03:10 -0300

    tableview config

---
+++ MyList/View/Cell/MyListTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  MyListTableViewCell.swift
+//  MyList
+//
+//  Created by Gabriel de Castro Chaves on 24/10/23.
+//
+
+import UIKit
+
+class MyListTableViewCell: UITableViewCell {
+    
+    static let identifier = "MyListTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
