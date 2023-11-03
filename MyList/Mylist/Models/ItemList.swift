d229bd2823c9ae4ac6ba3c81d6b1813faf7ca4d3
Gabriel Castro <gabrieldcc@gmail.com>
2023-11-03 14:46:29 -0300

    Firebase configured

---
+++ MyList/Mylist/Models/ItemList.swift
@@ -0,0 +1,12 @@
+//
+//  ItemList.swift
+//  MyList
+//
+//  Created by Gabriel de Castro Chaves on 02/11/23.
+//
+
+import Foundation
+
+struct ItemList {
+    var index: String
+}
--- MyList/Mylist/Scenes/MyList/MyListViewController.swift
+++ MyList/Mylist/Scenes/MyList/MyListViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 final class MyListViewController: UIViewController {
     
@@ -22,7 +23,11 @@ final class MyListViewController: UIViewController {
     
     private func setupView() {
         navigationBar.topItem?.title = NavigationBarTitleEnum.myList.rawValue
-        itensList = MyListUserDefault.getObject(with: .itensList)
+        //itensList = MyListUserDefault.getObject(with: .itensList)
+        //itensList = getDataFirebaseFirestore()
+        getDataFirebaseFirestore()
+        print("LISTA DE ITENS ---> \(itensList)")
+        //addDataBaseDocumentInFirebase()
     }
     
     private func setupTableView() {
@@ -42,8 +47,9 @@ final class MyListViewController: UIViewController {
             leftButtonTitle: .cancel,
             rightButtonTitle: .delete) {
                 self.itensList.removeAll()
+                //MyListUserDefault.save(object: self.itensList, with: .itensList)
+                self.addItemInFirebaseFirestore("")
                 self.tableView.reloadData()
-                MyListUserDefault.save(object: self.itensList, with: .itensList)
             }
     }
     
@@ -56,10 +62,59 @@ final class MyListViewController: UIViewController {
             leftButtonTitle: .cancel,
             rightButtonTitle: .ok) { item in
                 self.itensList.append(item)
+                //MyListUserDefault.save(object: self.itensList, with: .itensList)
+                self.addItemInFirebaseFirestore(item)
                 self.tableView.reloadData()
-                MyListUserDefault.save(object: self.itensList, with: .itensList)
                 self.addBarButtonAction()
             }
+    }
+    
+    private func addItemInFirebaseFirestore(_ item: String) {
+        let dataBase = Firestore.firestore()
+        dataBase.collection("itensList").document("itensDocument")
+            .updateData([
+                "items": FieldValue.arrayUnion([item])
+            ]) { error in
+                if let error = error {
+                    print("Error updating document: \(error)")
+                } else {
+                    print("Document successfully updated!")
+                }
+            }
+        
+    }
+    
+    private func deleteItemInFirebaseFirestore(_ item: String) {
+        let dataBase = Firestore.firestore()
+        dataBase.collection("itensList").document("itensDocument")
+           .updateData([
+               "items": FieldValue.arrayUnion([item])
+           ]) { error in
+               if let error = error {
+                   print("Error updating document: \(error)")
+               } else {
+                   print("Document successfully updated!")
+               }
+           }
+
+    }
+    
+    private func getDataFirebaseFirestore() {
+        let dataBase = Firestore.firestore()
+        dataBase.collection("itensList").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    let data = document.data()
+                    guard let items = data["items"] as? [String] else { return }
+                    items.forEach { value in
+                        self.itensList.append(value)
+                        self.tableView.reloadData()
+                    }
+                }
+            }
+        }
     }
     
     @IBAction func didTapDeleteBarButton(_ sender: UIBarButtonItem) {
@@ -90,7 +145,7 @@ extension MyListViewController: UITableViewDelegate, UITableViewDataSource {
         ) {  _, _, _ in
             self.itensList.remove(at: indexPath.row)
             tableView.reloadData()
-            MyListUserDefault.save(object: self.itensList, with: .itensList)
+            //MyListUserDefault.save(object: self.itensList, with: .itensList)
         }
         trash.backgroundColor = .systemRed
         let configuration = UISwipeActionsConfiguration(actions: [trash])
